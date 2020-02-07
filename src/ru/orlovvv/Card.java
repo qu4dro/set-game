291553b261d23b40f34cd37ba6701aa4f852614d
qu4dro <qu4dro.sl@gmail.com>
2020-02-07 13:30:20 +0800

    added card class

---
+++ src/ru/orlovvv/Card.java
@@ -0,0 +1,49 @@
+package ru.orlovvv;
+
+public class Card {
+
+    private int fill;
+
+    private int count;
+
+    private int shape;
+
+    private int color;
+
+    public Card(final int fill, final int count, final int shape, final int color) {
+        this.fill = fill;
+        this.count = count;
+        this.shape = shape;
+        this.color = color;
+    }
+
+
+    @Override
+    public String toString() {
+        return "fill: " + fill + "count: " + count + ", shape: " + shape + ", color: " + color;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return fill == card.fill &&
+                count == card.count &&
+                shape == card.shape &&
+                color == card.color;
+    }
+
+    public Card getThird(final Card firstCard, final Card secondCard) {
+        return new Card(getThirdValue(firstCard.fill, secondCard.fill),
+                getThirdValue(firstCard.count, secondCard.count),
+                getThirdValue(firstCard.shape, secondCard.shape),
+                getThirdValue(firstCard.color, secondCard.color)
+        );
+    }
+
+    private int getThirdValue(final int i, final int j) {
+        return i == j ? i : 6 - (i + j);
+    }
+}
